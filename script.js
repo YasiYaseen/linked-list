b43a4e1f596e36b1df4103e74f3608ef84ee9d38
yaseen <yasikalleri@gmail.com>
2023-12-28 23:01:10 +0530

    all functionalities added

--- script.js
+++ script.js
@@ -14,66 +14,210 @@
 // pop removes the last element from the list
 // contains(value) returns true if the passed in value is in the list and otherwise returns false.
 // find(value) returns the index of the node containing value, or null if not found.
-// toString represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+// toString represents your LinkedList objects as strings, so you can print them out and preview them in the console.
+//   The format should be: ( value ) -> ( value ) -> ( value ) -> null
+
 // Extra credit
 // insertAt(value, index) that inserts a new node with the provided value at the given index.
 // removeAt(index) that removes the node at the given index.
 // Extra Credit Tip: When you insert or remove a node, consider how it will affect the existing nodes. Some of the nodes will need their nextNode link updated.
 
-class Node{
-constructor(value=null,next=null){
-    this.value=value;
-    this.next=next;
+class Node {
+  constructor(value = null, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
-}
 
-class LinkedList{
-    constructor(){
-        this.head=null;
-        this.tail=null;
-
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  append(value) {
+    let newNode = new Node(value);
+    if (this.head != null) {
+      this.head.next = newNode;
+    } else {
+      this.tail = newNode;
     }
-    append(value){
-        let newNode = new Node(value);
-        if(this.head != null){
-            this.head.next = newNode;
-            
-        }else{
-            this.tail=newNode;
-        }
-        this.head=newNode;
-        console.log(newNode.value);
+    this.head = newNode;
+    console.log(newNode.value);
+  }
+
+  prepend(value) {
+    let newNode = new Node(value);
+    if (this.tail != null) {
+      newNode.next = this.tail;
     }
-
-    prepend(value){
-        let newNode = new Node(value);
-        if(this.tail != null){
-            newNode.next = this.tail;
-
-        }
-        this.tail=newNode;
-        console.log(newNode.value);
-
-
+    this.tail = newNode;
+    console.log(newNode.value);
+  }
+  size() {
+    if (this.tail == null) {
+      return 0;
+    } else {
+      let temp = this.tail;
+      let size = 0;
+      while (temp != null) {
+        size++;
+        temp = temp.next;
+      }
+      return size;
     }
-    size(){
-        if(this.tail==null){
-            return 0;
-        }else{
-            let temp = this.tail;
-            let size =0;
-            while(temp.next != null){
-                size++;
-                temp=temp.next;
-            }
-            return (size+1);
-        }
+  }
+
+  at(index) {
+    let temp = this.tail;
+    let result = null;
+    for (let i = 0; temp != null; i++) {
+      if (i == index) {
+        result = temp;
+        break;
+      }
+      temp = temp.next;
     }
+    return result;
+  }
+  pop() {
+    let temp = this.tail;
+    let deleted = null;
+    if (temp == null) {
+      return null;
+    }
+    if (temp.next == null) {
+      this.tail = this.head = null;
+      return temp;
+    }
+    while (temp != null) {
+      if (temp.next.next == null) {
+        deleted = temp.next;
+        temp.next = null;
+        this.head = temp;
+      }
+      temp = temp.next;
+    }
+    return deleted;
+  }
+  contains(value) {
+    let temp = this.tail;
+    while (temp != null) {
+      if (temp.value == value) {
+        return true;
+      }
+      temp = temp.next;
+    }
+    return false;
+  }
+  find(value) {
+    let index = 0;
+    let temp = this.tail;
+    while (temp != null) {
+      if (temp.value == value) {
+        return index;
+      }
+      temp = temp.next;
+      index++;
+    }
+    return null;
+  }
+
+  toString() {
+    let temp = this.tail;
+    let string = "";
+    if (temp == null) return "null";
+
+    while (temp != null) {
+      string += `${temp.value} -> `;
+      temp = temp.next;
+    }
+    string += "null";
+    return string;
+  }
+  insertAt(value, index) {
+   let current = this.tail;
+   let prev =current;
+    let i = 0;
+    const node = new Node(value);
+    if (index < 1) {
+      node.next = this.tail;
+      this.tail = node;
+      return "added at the first";
+    }
+    while (current != null) {
+      if (i == index) {
+        node.next = current;
+        prev.next = node;
+        return `added at the ${i}th index `;
+      }
+      prev = current;
+      current = current.next;
+      i++;
+    }
+    if (prev == this.head) {
+      prev.next = node;
+      this.head = node;
+      return `added at  last `;
+    }
+  }
+
+  removeAt(index){
+    let prev = this.tail;
+    let current = this.tail;
+    let i =0;
+    if(index==0){
+      this.tail =current.next;
+      if(current==this.head)this.head=null;
+      return `deleted value : ${current.value}`;
+    
+    }
+    
+    while(current != null){
+
+      if(i==index){
+        prev.next = current.next;
+        if(current==this.head)this.head =prev;
+        return `deleted value : ${current.value}`;
+
+      }
+      prev =current;
+      current=current.next;
+      i++;
+    }
+      throw Error('Undefined index provided');
+  }
+
 }
 const linkedList = new LinkedList();
+// console.log(linkedList.tail);
+console.log(linkedList.pop());
+
 linkedList.append(3);
 linkedList.append(4);
 linkedList.append(2);
 linkedList.append(1);
-console.log("Size : "+linkedList.size());
-console.log(linkedList.tail);
+console.log("adding to an index started");
+
+console.log(linkedList.insertAt(10,10));
+console.log(linkedList.insertAt(5,0));
+console.log(linkedList.insertAt(6,3));
+console.log(linkedList.insertAt(8,6));
+console.log(linkedList.toString());
+
+console.log(linkedList.removeAt(1));
+console.log(linkedList.removeAt(5));
+console.log(linkedList.toString());
+// console.log(linkedList.contains(3));
+// console.log(linkedList.find(2));
+// console.log(linkedList.find(8));
+
+// console.log(linkedList.head);
+// console.log("Size : " + linkedList.size());
+// console.log(linkedList.at(2));
+// console.log(linkedList.pop());
+// console.log(linkedList.pop());
+// console.log(linkedList.pop());
+// console.log(linkedList.pop());
+// console.log(linkedList.pop());
+
+// console.log("Size : " + linkedList.size());
